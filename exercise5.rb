970c35d9da9e380a06ba2dc06d1bb618db31f1ed
Marlon O'Neil <hello@marlononeil.com>
2017-08-09 21:13:13 -0400

    Exercise 5 step 2 completed

--- exercise5.rb
+++ exercise5.rb
@@ -5,14 +5,17 @@ def greet_backwards(user_name)
   #that greets people using their reversed names.
   reversed = "Hello, #{user_name.reverse}! Welcome home"
 
-
 end
 
 
-print "Hey there stranger, what is your name? "
-user_name = gets.chomp
-result = greet_backwards(user_name)
+# print "Hey there stranger, what is your name? "
+# user_name = gets.chomp
+# result = greet_backwards(user_name)
 
-puts result
+
 
 #Call that method for four different people: "Bob", "Shirly", "Sue", and "Andy".
+puts greet_backwards("Bob")
+puts greet_backwards("Shirly")
+puts greet_backwards("Sue")
+puts greet_backwards("Andy")
