f2e2f08923fe7e66ea774f351585176073852fe6
Marlon O'Neil <hello@marlononeil.com>
2017-08-09 21:32:51 -0400

    Exercise 7 step 1

---
+++ exercise7.rb
@@ -0,0 +1,9 @@
+#Let's create a method wrap_text that wraps text in symbols of our choice.
+def wrap_text(text, symbol)
+
+end
+
+print "Please enter the text you wish to make pretty: "
+text = gets.chomp
+print "Sweet! Now enter the symbols you wish to attach to the text: "
+symbol = gets.chomp.to_i
