831ae0d1c5468c3662a49757e875578145bc9364
Marlon O'Neil <hello@marlononeil.com>
2017-08-09 20:38:49 -0400

    exercise 1 completed

---
+++ exercise1.rb
@@ -0,0 +1,10 @@
+#Define a method called double that accepts an argument called my_number
+def double (number)
+  #returns that number multiplied by 2.
+  number * 2
+end
+
+#Try calling it multiple times and pass in different numbers each time.
+puts double(40)
+puts double(100)
+puts double(1223.2)
