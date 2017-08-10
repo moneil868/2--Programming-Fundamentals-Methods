c9bd7364e1858eac37f6b0f6289b365b45fb550b
Marlon O'Neil <hello@marlononeil.com>
2017-08-09 20:49:47 -0400

    completed Exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,19 @@
+#Define a method called negative? that accepts a number as an argument
+def negative(number)
+
+  #returns a boolean (true/false) indicating whether that number is negative or not.
+  number < 0
+end
+
+#Try calling it multiple times, passing in different numbers each time.
+print "Please enter the number you wish to evalute: "
+number = gets.chomp.to_i
+result = negative(number)
+
+puts result
+
+if result == false
+  puts "The number #{number} is not a negative number"
+else
+  puts "The number #{number} is a negative number"
+end
