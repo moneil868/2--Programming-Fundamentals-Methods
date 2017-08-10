95576467be0b63b546152901dd3dc01d450c89f3
Marlon O'Neil <hello@marlononeil.com>
2017-08-09 21:28:10 -0400

    completed exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,15 @@
 #Create a method that converts Fahrenheit temperatures to Celsius in a file called exercise5.rb.
-def convert_fahrenheit_to_celsius
+def convert_fahrenheit_to_celsius(f_value)
+
+  #do the conversion with this formula: C = (F - 32) x 5/9
+  c_value = (f_value - 32) * 5/9
+
 end
+
+print "Please enter the temperture in Fahrenheit you wish to convert: "
+f_value = gets.chomp.to_i
+
+result = convert_fahrenheit_to_celsius(f_value)
+
+#Output the result in a full sentence using string interpolation.
+puts "#{f_value} degrees Celsius has been converted to #{result} degrees Fahrenheit"
